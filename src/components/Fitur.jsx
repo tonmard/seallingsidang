defed811d3c21296f5a636442c25367acee1d948
Tony Mardyansyah <tonymardyansyah78@gmail.com>
2024-06-09 13:27:08 +0700

    mengubah cursor

--- src/components/Fitur.jsx
+++ src/components/Fitur.jsx
@@ -9,8 +9,8 @@ const Fitur = () => {
           Keunggulan Layanan Kami
         </h1>
       </div>
-      <div className="max-w-[1240px] cursor-grabbing mx-auto grid md:grid-cols-3 gap-8">
-        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
+        <div className="w-full shadow-xl cursor-grabbing flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
           <img className="w-40 mx-auto mt-[-3rem]" src={jam} alt="/" />
           <h2 className="text-2xl font-bold text-center py-8 ">Real Time</h2>
           <div className="text-center font-medium">
@@ -19,7 +19,7 @@ const Fitur = () => {
             </p>
           </div>
         </div>
-        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+        <div className="w-full shadow-xl cursor-grabbing flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
           <img className="w-40 mx-auto mt-[-3rem]" src={bintang} alt="/" />
           <h2 className="text-2xl font-bold text-center py-8">Verifikasi</h2>
           <div className="text-center font-medium">
@@ -28,7 +28,7 @@ const Fitur = () => {
             </p>
           </div>
         </div>
-        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+        <div className="w-full shadow-xl cursor-grabbing flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
           <img className="w-40 mx-auto mt-[-3rem]" src={alarm} alt="/" />
           <h2 className="text-2xl font-bold text-center py-8">
             Early Warning System
