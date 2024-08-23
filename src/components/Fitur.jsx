f13273bbabeaf78d3f9cbf6fc79c3ddf2928acb0
Tony <tonymardyansyah78@gmail.com>
2024-08-23 07:12:48 +0700

    interval time

--- src/components/Fitur.jsx
+++ src/components/Fitur.jsx
@@ -13,7 +13,9 @@ const Fitur = () => {
         <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-12 md:px-16 px-4">
           <div className="w-full shadow-xl cursor-grabbing flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
             <img className="w-40 mx-auto mt-[-3rem]" src={jam} alt="Jam" />
-            <h2 className="text-2xl font-bold text-center py-8 ">Real Time</h2>
+            <h2 className="text-2xl font-bold text-center py-8 ">
+              Interval Time
+            </h2>
             <div className="text-center font-medium">
               <p className="py-2 border-t mx-8 mt-1">
                 Data diperbarui setiap 15 menit dan tervalidasi.
